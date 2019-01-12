7a36801ace594e23c5ea22aadf3dc5d641f07584
Rekt77 <timtaeil@gmail.com>
2019-01-12 20:12:07 +0900

    file modified : container/TodoInputContainer.js

--- bs4-react-todo/src/containers/TodoInputContainer.js
+++ bs4-react-todo/src/containers/TodoInputContainer.js
@@ -46,6 +46,6 @@ export default connect(
     }),
     (dispatch) => ({
         InputActions: bindActionCreators(inputActions, dispatch),
-        TodosActions: bindActionCreators(todoActions, dispatch)
+        TodosActions: bindActionCreators(todosActions, dispatch)
     })
 )(TodoInputContainer);
